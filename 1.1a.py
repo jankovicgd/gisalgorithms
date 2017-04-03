8467352b6cf4431490495fce24029c27327ed07f
Nikola Jankovic <jankovic.gd@gmail.com>
2017-04-03 11:54:08 +0200

    Comments for 1.1a

--- 1.1a.py
+++ 1.1a.py
@@ -1,16 +1,21 @@
+# Computational concerns for algorithms
+# Listing 1.1: Linear search to find point in a list
 
+# Initialize an empty list
 l = []
 
+# Fill the list l with data by appending
 for x in range (30,50,5):
     for y in range (1,10,3):
         l.append((x,y))
 
+# Test print
 print(l)
 
-p = (31, 1)
+# The point searched for
+p = (30, 1)
 
-print(p)
-
+# Search for the point in a list
 for point in l:
     if point == p:
         print("Found point")
