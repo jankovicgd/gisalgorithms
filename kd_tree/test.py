eb27ae1285685f0ae36ffefe39039dd9f28de111
Nikola Jankovic <jankovic.gd@gmail.com>
2017-04-03 15:43:37 +0200

    Documenttion

--- kd_tree/test.py
+++ kd_tree/test.py
@@ -1,9 +1,10 @@
+# 1.1 Computational concerns for algorithms
+# Listing 1.3: Binary search to find point p0 in a tree
+
+# Import from kd_node
 from kd_node import *
 
-# pts = []
-# for i in range(7):
-#     pts.append(Point())
-
+# Define points
 pts =[Point(6,7), Point(4,6), Point(9,4), Point(2,3), Point(3,7), Point(7,4), Point(9,6)]
 print(str(pts))
 
