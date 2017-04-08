7bac6e8e5fd7a7c7f2342fc0f0ba23a73a81a1ce
jankovicgd <jankovic.gd@gmail.com>
2017-04-08 13:44:30 +0200

    documentation for function

--- listing28.py
+++ listing28.py
@@ -6,4 +6,12 @@ import math
 from geometries.point import *
 
 def pip_cross(point, pgon):
-    pass
+    """
+    Determines whether a point is in a polygon.
+    Code adopted from the C program in Graphics Gems IV by Haines (1994).
+    Input:
+        pgon: a list of points as the vertices for a polygon
+        point: the point
+    Output:
+        Returns a boolean value of True or False and the number of times the half-line crosses the polygon boundary
+    """
