66b472cd317fde801efb326d33317e1ae263bb14
jankovicgd <jankovic.gd@gmail.com>
2017-04-08 16:01:36 +0200

    finished listing28

--- listing28.py
+++ listing28.py
@@ -1,5 +1,5 @@
-# 2.6 Intersection of two line segments
-# Listing 2.7: Calculating the intersection between two line segments
+# 2.7 Point-in-polygon operation
+# Listing 2.8: The even-odd algorithm for the Point-in-polygon test
 
 # Imports
 import math
@@ -15,3 +15,47 @@ def pip_cross(point, pgon):
     Output:
         Returns a boolean value of True or False and the number of times the half-line crosses the polygon boundary
     """
+    numvert = len(pgon)
+    tx = point.x
+    ty = point.y
+    p1 = pgon[numvert - 1]
+    p2 = pgon[0]
+    yflag1 = (p1.y >= ty)           # p1 on or above point
+    crossing = 0
+    inside_flag = 0
+    for j in range(numvert - 1):
+        yflag2 = (p2.y >= ty)       # p2 on or above point
+        if yflag1 != yflag2:        # both sides of half-line
+            xflag1 = (p1.x >= tx)   # left-right side of p1
+            xflag2 = (p2.x >= tx)   # left-right side of p2
+            if xflag2 == xflag1:    # both points on right side
+                if xflag1:
+                    crossing += 1
+                    inside_flag = not inside_flag
+                else:
+                    m = p2.x - float((p2.y - ty)) *\
+                        (p1.x - p2.x) / (p1.y - p2.y)
+                    if m >= tx:
+                        crossing += 1
+                        inside_flag = not inside_flag
+        yflag1 = yflag2
+        p1 = p2
+        p2 = pgon[j + 1]
+    return inside_flag, crossing
+
+if __name__ == "__main__":
+    points = [[0, 10], [5, 0], [10, 10], [15, 0], [20, 10],
+              [25, 0], [30, 20], [40, 20], [45, 0], [50, 50],
+              [40, 40], [30, 50], [25, 20], [20, 50], [15, 10],
+              [10, 50], [8, 8], [4, 50], [0, 10]]
+    ppgon = [Point(p[0], p[1]) for p in points]
+    inout = lambda pip: "IN" if pip is True else "OUT"
+    point = Point(10, 30)
+
+    print("Point {} is {}".format(point, inout(pip_cross(point, ppgon)[0])))
+    point = Point(10, 20)
+    print("Point {} is {}".format(point, inout(pip_cross(point, ppgon)[0])))
+    point = Point(20, 40)
+    print("Point {} is {}".format(point, inout(pip_cross(point, ppgon)[0])))
+    point = Point(5, 40)
+    print("Point {} is {}".format(point, inout(pip_cross(point, ppgon)[0])))
