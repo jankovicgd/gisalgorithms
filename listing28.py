bcb4e4315e93986474c08953533b288b04a3f735
jankovicgd <jankovic.gd@gmail.com>
2017-04-08 13:37:32 +0200

    skeleton listing28

---
+++ listing28.py
@@ -0,0 +1,9 @@
+# 2.6 Intersection of two line segments
+# Listing 2.7: Calculating the intersection between two line segments
+
+# Imports
+import math
+from geometries.point import *
+
+def pip_cross(point, pgon):
+    pass
